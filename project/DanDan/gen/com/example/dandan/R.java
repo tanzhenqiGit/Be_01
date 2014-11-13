d5615ecda55b9a81ebd33c5cd00dcd9d82c68093
tanzhenqi <tan_zhen_qi@163.com>
2014-11-13 21:13:51 +0800

    add for content_Provider.

--- project/DanDan/gen/com/example/dandan/R.java
+++ project/DanDan/gen/com/example/dandan/R.java
@@ -27,40 +27,43 @@ public final class R {
     public static final class drawable {
         public static final int dandan=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int line=0x7f020002;
+        public static final int icon_dan=0x7f020002;
+        public static final int line=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090014;
-        public static final int delete=0x7f09000e;
-        public static final int insert=0x7f09000b;
+        public static final int action_settings=0x7f090015;
+        public static final int delete=0x7f09000f;
+        public static final int insert=0x7f09000c;
         public static final int listView=0x7f090000;
         public static final int nine_chapter_contests=0x7f090001;
-        public static final int nine_content_provider_add=0x7f09000f;
-        public static final int nine_content_provider_email_text=0x7f090013;
-        public static final int nine_content_provider_name_text=0x7f090011;
-        public static final int nine_content_provider_number_text=0x7f090012;
-        public static final int nine_content_provider_search=0x7f090010;
-        public static final int nine_dict_resolver_line_detail=0x7f090003;
-        public static final int nine_dict_resolver_line_word=0x7f090002;
-        public static final int nine_dict_resolver_main_detail=0x7f090005;
-        public static final int nine_dict_resolver_main_insert=0x7f090006;
-        public static final int nine_dict_resolver_main_key=0x7f090007;
-        public static final int nine_dict_resolver_main_search=0x7f090008;
-        public static final int nine_dict_resolver_main_show=0x7f090009;
-        public static final int nine_dict_resolver_main_word=0x7f090004;
-        public static final int nine_dict_resolver_result_show=0x7f09000a;
-        public static final int query=0x7f09000d;
-        public static final int update=0x7f09000c;
+        public static final int nine_content_provider_add=0x7f090010;
+        public static final int nine_content_provider_email_text=0x7f090014;
+        public static final int nine_content_provider_name_text=0x7f090012;
+        public static final int nine_content_provider_number_text=0x7f090013;
+        public static final int nine_content_provider_result_list=0x7f090002;
+        public static final int nine_content_provider_search=0x7f090011;
+        public static final int nine_dict_resolver_line_detail=0x7f090004;
+        public static final int nine_dict_resolver_line_word=0x7f090003;
+        public static final int nine_dict_resolver_main_detail=0x7f090006;
+        public static final int nine_dict_resolver_main_insert=0x7f090007;
+        public static final int nine_dict_resolver_main_key=0x7f090008;
+        public static final int nine_dict_resolver_main_search=0x7f090009;
+        public static final int nine_dict_resolver_main_show=0x7f09000a;
+        public static final int nine_dict_resolver_main_word=0x7f090005;
+        public static final int nine_dict_resolver_result_show=0x7f09000b;
+        public static final int query=0x7f09000e;
+        public static final int update=0x7f09000d;
     }
     public static final class layout {
         public static final int activity_main_interface=0x7f030000;
         public static final int contents_activity=0x7f030001;
         public static final int nine_chapter_contents=0x7f030002;
-        public static final int nine_dict_resolver_line=0x7f030003;
-        public static final int nine_dict_resolver_main=0x7f030004;
-        public static final int nine_dict_resolver_result=0x7f030005;
-        public static final int nine_first_content=0x7f030006;
-        public static final int ninie_content_provider=0x7f030007;
+        public static final int nine_content_provider_result=0x7f030003;
+        public static final int nine_dict_resolver_line=0x7f030004;
+        public static final int nine_dict_resolver_main=0x7f030005;
+        public static final int nine_dict_resolver_result=0x7f030006;
+        public static final int nine_first_content=0x7f030007;
+        public static final int ninie_content_provider=0x7f030008;
     }
     public static final class menu {
         public static final int main_interface=0x7f080000;
--- project/DanDan/src/com/example/contents/nine/contentprovider/ContentProviderTest.java
+++ project/DanDan/src/com/example/contents/nine/contentprovider/ContentProviderTest.java
@@ -6,13 +6,20 @@ import com.example.dandan.R;
 
 import android.app.Activity;
 import android.database.Cursor;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.AbsListView;
+import android.widget.AbsListView.LayoutParams;
 import android.widget.Button;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 public class ContentProviderTest extends Activity {
 
@@ -52,7 +59,8 @@ public class ContentProviderTest extends Activity {
 				{
 					String contactId = cursor.getString(cursor.getColumnIndex(
 							ContactsContract.Contacts._ID));
-					String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+					String name = cursor.getString(cursor.getColumnIndex(
+							ContactsContract.Contacts.DISPLAY_NAME));
 					names.add(name);
 					Log.d(TAG,"name = " +name);
 					Log.d(TAG, "ID = " + contactId);
@@ -79,10 +87,143 @@ public class ContentProviderTest extends Activity {
 							null, null);
 					
 					while(emails.moveToNext()) {
-						String emailAddress = emails.getString(emails.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+						String emailAddress = emails.getString(emails.getColumnIndex(
+								ContactsContract.CommonDataKinds.Email.DATA));
+						detail.add("ÓÊ¼þµØÖ·£º" + emailAddress);
 						
 					}
+					emails.close();
+					details.add(detail);
 				}
+				cursor.close();
+				
+				View resultDialog = getLayoutInflater()
+						.inflate(R.layout.nine_content_provider_result, null);
+				ExpandableListView list = (ExpandableListView) resultDialog
+						.findViewById(R.id.nine_content_provider_result_list);
+				ExpandableListAdapter adapter = new ExpandableListAdapter() {
+					
+					private TextView getTextView()
+					{
+						AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
+								ViewGroup.LayoutParams.MATCH_PARENT,64);
+						//TextView textView = new TextView(Contact
+						//		)
+						return null;
+					}
+					@Override
+					public void unregisterDataSetObserver(DataSetObserver observer) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void registerDataSetObserver(DataSetObserver observer) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onGroupExpanded(int groupPosition) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onGroupCollapsed(int groupPosition) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public boolean isEmpty() {
+						// TODO Auto-generated method stub
+						return false;
+					}
+					
+					@Override
+					public boolean isChildSelectable(int groupPosition, int childPosition) {
+						// TODO Auto-generated method stub
+						return false;
+					}
+					
+					@Override
+					public boolean hasStableIds() {
+						// TODO Auto-generated method stub
+						return false;
+					}
+					
+					@Override
+					public View getGroupView(int groupPosition, boolean isExpanded,
+							View convertView, ViewGroup parent) {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					
+					@Override
+					public long getGroupId(int groupPosition) {
+						// TODO Auto-generated method stub
+						return 0;
+					}
+					
+					@Override
+					public int getGroupCount() {
+						// TODO Auto-generated method stub
+						return 0;
+					}
+					
+					@Override
+					public Object getGroup(int groupPosition) {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					
+					@Override
+					public long getCombinedGroupId(long groupId) {
+						// TODO Auto-generated method stub
+						return 0;
+					}
+					
+					@Override
+					public long getCombinedChildId(long groupId, long childId) {
+						// TODO Auto-generated method stub
+						return 0;
+					}
+					
+					@Override
+					public int getChildrenCount(int groupPosition) {
+						Log.d(TAG, "getChildrenCount groupPosition:" + groupPosition);
+						
+						return details.get(groupPosition).size();
+					}
+					
+					@Override
+					public View getChildView(int groupPosition, int childPosition,
+							boolean isLastChild, View convertView, ViewGroup parent) {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					
+					@Override
+					public long getChildId(int groupPosition, int childPosition) {
+						Log.d(TAG, "getChildId childPosition" +
+							childPosition +"groupPosition:" + groupPosition);
+						return childPosition;
+					}
+					
+					@Override
+					public Object getChild(int groupPosition, int childPosition) {
+						Log.d(TAG, "getChind groupPosition:"
+					        + groupPosition + ",childPosition:" + childPosition);
+						return details.get(groupPosition).get(childPosition);
+					}
+					
+					@Override
+					public boolean areAllItemsEnabled() {
+						// TODO Auto-generated method stub
+						return false;
+					}
+				};
 			}
 		});
 	}
