ce21d5308b44397126140b80909f57ef9b72e2c5
tanzhenqi <tan_zhen_qi@163.com>
2014-11-15 16:27:13 +0800

    add for complete ContactProviderTest.

--- project/DanDan/src/com/example/contents/nine/contentprovider/ContentProviderTest.java
+++ project/DanDan/src/com/example/contents/nine/contentprovider/ContentProviderTest.java
@@ -5,21 +5,33 @@ import java.util.ArrayList;
 import com.example.dandan.R;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.DataSetObserver;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Email;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.CommonDataKinds.StructuredName;
 import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.Data;
+import android.provider.ContactsContract.RawContacts;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.AbsListView;
 import android.widget.AbsListView.LayoutParams;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ContentProviderTest extends Activity {
 
@@ -107,10 +119,14 @@ public class ContentProviderTest extends Activity {
 					{
 						AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
 								ViewGroup.LayoutParams.MATCH_PARENT,64);
-						//TextView textView = new TextView(Contact
-						//		)
-						return null;
+						TextView textView = new TextView(ContentProviderTest.this);
+						textView.setLayoutParams(lp);
+						textView.setGravity(Gravity.CENTER_VERTICAL|Gravity.LEFT);
+						textView.setPadding(36, 0, 0, 0);
+						textView.setTextSize(20);
+						return textView;
 					}
+					
 					@Override
 					public void unregisterDataSetObserver(DataSetObserver observer) {
 						// TODO Auto-generated method stub
@@ -143,39 +159,46 @@ public class ContentProviderTest extends Activity {
 					
 					@Override
 					public boolean isChildSelectable(int groupPosition, int childPosition) {
-						// TODO Auto-generated method stub
-						return false;
+						Log.d(TAG, "ischildSelectable groupPosition=" 
+							+ groupPosition + "childPosition=" + childPosition);
+						
+						return true;
 					}
 					
 					@Override
 					public boolean hasStableIds() {
-						// TODO Auto-generated method stub
-						return false;
+						Log.d(TAG, "hasStableIds");
+						return true;
 					}
 					
 					@Override
 					public View getGroupView(int groupPosition, boolean isExpanded,
 							View convertView, ViewGroup parent) {
-						// TODO Auto-generated method stub
-						return null;
+						Log.d(TAG, "getGroupView groupPosition="+groupPosition);
+						TextView textView = getTextView();
+						textView.setText(getGroup(groupPosition).toString());
+						return textView;
 					}
 					
 					@Override
 					public long getGroupId(int groupPosition) {
-						// TODO Auto-generated method stub
-						return 0;
+						Log.d(TAG, "getGroupId groupPosition=" + groupPosition);
+						
+						return groupPosition;
 					}
 					
 					@Override
 					public int getGroupCount() {
-						// TODO Auto-generated method stub
-						return 0;
+						Log.d(TAG,"getGroupCount is called");
+						
+						return names.size();
 					}
 					
 					@Override
 					public Object getGroup(int groupPosition) {
-						// TODO Auto-generated method stub
-						return null;
+						Log.d(TAG, "getGroup groupPosition=" + groupPosition);
+						
+						return names.get(groupPosition);
 					}
 					
 					@Override
@@ -200,9 +223,13 @@ public class ContentProviderTest extends Activity {
 					@Override
 					public View getChildView(int groupPosition, int childPosition,
 							boolean isLastChild, View convertView, ViewGroup parent) {
-						// TODO Auto-generated method stub
-						return null;
+						Log.d(TAG, "getChildView groupPosition=" 
+							+ groupPosition + ",childPosition=" + childPosition);
+						TextView textView = getTextView();
+						textView.setText(getChild(groupPosition, childPosition).toString());
+						return textView;
 					}
+					
 					
 					@Override
 					public long getChildId(int groupPosition, int childPosition) {
@@ -224,8 +251,51 @@ public class ContentProviderTest extends Activity {
 						return false;
 					}
 				};
+				list.setAdapter(adapter);
+				new AlertDialog.Builder(ContentProviderTest.this)
+				   .setView(resultDialog).setPositiveButton("确定", null)
+				   .show();
 			}
 		});
+		mAddBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String name = ((EditText) findViewById(R.id.nine_content_provider_name_text))
+						.getText().toString();
+				String phone = ((EditText) findViewById(R.id.nine_content_provider_number_text))
+						.getText().toString();
+				String email = ((EditText) findViewById(R.id.nine_content_provider_email_text))
+						.getText().toString();
+				ContentValues values = new ContentValues();
+				Uri rawContactUri = getContentResolver().insert(RawContacts.CONTENT_URI, values);
+				long rawContactId = ContentUris.parseId(rawContactUri);
+				values.clear();
+				values.put(Data.RAW_CONTACT_ID, rawContactId);
+				values.put(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
+				values.put(StructuredName.GIVEN_NAME, name);
+				getContentResolver().insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
+				values.clear();
+				
+				values.put(Data.RAW_CONTACT_ID, rawContactId);
+				values.put(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
+				values.put(Phone.NUMBER, phone);
+				values.put(Phone.TYPE, Phone.TYPE_MOBILE);
+				getContentResolver().insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
+				
+				values.clear();
+				values.put(Data.RAW_CONTACT_ID, rawContactId);
+				values.put(Data.MIMETYPE, Email.CONTENT_ITEM_TYPE);
+				values.put(Email.DATA, email);
+				values.put(Email.TYPE, Email.TYPE_WORK);
+				getContentResolver().insert(android.provider.ContactsContract.Data.CONTENT_URI, values);
+				Toast.makeText(ContentProviderTest.this, "联系人数据添加成功", Toast.LENGTH_LONG).show();
+				
+				
+				
+			}
+		});
+	
 	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
