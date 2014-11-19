54846f43bde20608afeeb23b21bc70172af11f51
tanzhenqi <tanzhenqi@pset.suntec.net>
2014-11-19 11:42:34 +0800

    add for icon.
    Change-Id: If32daa8fbc903090106eaa177357c81f44032a44

--- project/DanDan/gen/com/example/dandan/R.java
+++ project/DanDan/gen/com/example/dandan/R.java
@@ -36,10 +36,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int android=0x7f020000;
-        public static final int dandan=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int icon_dan=0x7f020003;
-        public static final int line=0x7f020004;
+        public static final int bookmark=0x7f020001;
+        public static final int box=0x7f020002;
+        public static final int dandan=0x7f020003;
+        public static final int earth=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int icon_dan=0x7f020006;
+        public static final int line=0x7f020007;
+        public static final int music=0x7f020008;
+        public static final int phonebook=0x7f020009;
+        public static final int setting=0x7f02000a;
+        public static final int store=0x7f02000b;
     }
     public static final class id {
         public static final int action_settings=0x7f09002e;
